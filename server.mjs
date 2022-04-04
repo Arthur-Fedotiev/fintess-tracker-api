a6c16775cd7dc59350bf404ffc9304b77108c8ff
artf <artur.fedotiew@gmail.com>
2022-04-04 18:33:11 +0300

    added eslint and prettier

--- server.mjs
+++ server.mjs
@@ -1,22 +1,22 @@
-import dotenv from "dotenv";
-import express from "express";
-import { getDirName } from "./utils/path/dirName.mjs";
-import path from "path";
+import dotenv from 'dotenv'
+import express from 'express'
+import path from 'path'
+import { getDirName } from './utils/path/dirName.mjs'
 
 /**
  * Load environment variables
  */
 dotenv.config({
-  path: path.join(getDirName(import.meta.url), "config", "config.env"),
-});
+    path: path.join(getDirName(import.meta.url), 'config', 'config.env'),
+})
 
-const app = express();
+const app = express()
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 
 app.listen(
-  PORT,
-  console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
-  )
-);
+    PORT,
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+    )
+)
