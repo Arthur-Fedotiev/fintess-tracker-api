74f2534f2d970cd0ebe3ca98661f472d50909a18
artf <artur.fedotiew@gmail.com>
2022-04-05 12:08:31 +0300

    changed config file name

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { CONFIG } from './config';
+import { ENV_CONFIG } from './env-config';
 
 const app = express();
 
@@ -8,10 +8,10 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.status(200).json({
     success: true,
-    message: `Here is process env vars: process.env.NODE_ENV=${CONFIG.env}`,
+    message: `Here is process env vars: process.env.NODE_ENV=${ENV_CONFIG.env}`,
   });
 });
 
-const PORT = CONFIG.port || 8080;
+const PORT = ENV_CONFIG.port || 8080;
 
-app.listen(PORT, () => console.log(`⚡ Server running in ${CONFIG.env} mode on port ${CONFIG.port}`));
+app.listen(PORT, () => console.log(`⚡ Server running in ${ENV_CONFIG.env} mode on port ${ENV_CONFIG.port}`));
