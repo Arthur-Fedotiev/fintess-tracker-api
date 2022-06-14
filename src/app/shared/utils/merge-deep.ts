f82fa440a5dfa60d7cc05a3d799bd35389445dfe
arturfedotiev <artur.fedotiew@gmail.com>
2022-06-14 15:08:09 +0300

    implemented isObject util; improved PATCH err handling

--- src/app/shared/utils/merge-deep.ts
+++ src/app/shared/utils/merge-deep.ts
@@ -1,7 +1,14 @@
+import { BadRequestException } from '../models/error/bad-request';
+import { isObject } from './is-object';
+
 export const mergeDeep = (
   targetObject: Record<string, any>,
   mergedObject: Record<string, any>,
 ) => {
+  if (!(isObject(targetObject) && isObject(mergedObject))) {
+    throw new BadRequestException('Target and source must be simple objects');
+  }
+
   Object.keys(mergedObject).forEach((mergedObjectKey) => {
     const targetObjectValue = targetObject[mergedObjectKey];
     const mergedObjectValue = mergedObject[mergedObjectKey];
