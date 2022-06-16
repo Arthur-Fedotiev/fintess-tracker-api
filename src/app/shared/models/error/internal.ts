ac1bb8488b5f8f5c352a806bf74c33b46a79df21
Artur Fedotiev <artur.fedotiew@gmail.com>
2022-06-16 10:48:31 +0300

    Dev (#12)
    * covered use case of improper language param - fallback to english
    
    * implemented delete one execise route
    
    * changed TranslateService.translate signature
    
    *TranslateService.translate now expects data to be translated directly
    
    * exrecise GET API breacking changes
    
    *changed i18n implementation
    *allowed multiple languages selection by client
    *changed response DTO: added LanguageISO keys with nested maps
    *exercise repositoty get methods: i18n parameter made optional
    
    * implemented PATCH endpoint
    
    *implemented request handler
    *implemented UpdateExerciseCommand
    *implemented deepMerge helper
    
    * implemented isObject util; improved PATCH err handling
    
    * isObject util used in mergeDeep util
    
    * Feature/advanced querying and pagination (#9)
    
    * added exclusion error
    
    * allowed empty private constructors
    
    * implemented AdvancedResultsMongooseService; DB retrying
    
    * implemented optional pagination info
    
    * i18n results removed from GET many endpoint
    
    * i18nresults middleware removed from project
    
    * implemented logging functionality with winston (#11)

--- src/app/shared/models/error/internal.ts
+++ src/app/shared/models/error/internal.ts
@@ -2,7 +2,7 @@ import { HttpException } from './http-exception';
 
 export class InternalServerException extends HttpException {
   public statusCode = 500;
-  constructor() {
-    super('Server Error');
+  constructor(message: string = 'Server Error') {
+    super(message);
   }
 }
