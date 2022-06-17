8c87c078245fab58ef9144967b9c48ca5e2be11f
arturfedotiev <artur.fedotiew@gmail.com>
2022-06-17 07:17:28 +0300

    UnauthorizedException.status code changed to 401

--- src/app/shared/models/error/unauthorized.ts
+++ src/app/shared/models/error/unauthorized.ts
@@ -1,7 +1,7 @@
 import { HttpException } from './http-exception';
 
 export class UnauthorizedException extends HttpException {
-  public statusCode = 403;
+  public statusCode = 401;
   constructor(
     message: string = 'Unauthorized to access the resource. Please login at https://fitness-tracker-de06b.web.app/',
   ) {
