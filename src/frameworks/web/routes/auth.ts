2229da39fbba4d1e1bfdf49e56616d1456c57ada
arturfedotiev <artur.fedotiew@gmail.com>
2022-06-17 17:53:08 +0300

    implemented create user validation

--- src/frameworks/web/routes/auth.ts
+++ src/frameworks/web/routes/auth.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import { AuthController } from '../../../controllers/auth/auth.controller';
+import { CreateUserDTO } from '../../../controllers/auth/dto/create-user-dto';
 import { ProjectDependencies } from '../../../dependencies/project-dependencies.interface';
+import validationMiddleware from '../../common/error/validation.middleware';
 
 export const authRouter = (dependencies: ProjectDependencies) => {
   const router = express.Router();
 
   const controller = AuthController.getInstance(dependencies);
 
-  router.route('/signup').post(controller.signup);
+  router
+    .route('/signup')
+    .post(validationMiddleware(CreateUserDTO), controller.signup);
 
   return router;
 };
