a904e566059f4bf680d2264b771e5246f7eb3496
arturfedotiev <artur.fedotiew@gmail.com>
2022-06-27 06:52:58 +0300

    implemented gracefull shutdown

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,14 @@
-import { Express } from 'express';
-import { ENV_CONFIG } from './env-config';
-
 import projectDependencies from './dependencies/project-dependencies';
-import { closeServer } from './server/close-server';
+import { registerServerShutdownListeners } from './server/close-server';
 import { AppLogger } from './frameworks/common/log/winston-logger';
 import { createApp } from './server/create-app';
-
-const PORT = ENV_CONFIG.port;
+import { createServer } from './server/create-server-on-port';
 
 projectDependencies.DatabaseService.connect()
   .then(projectDependencies.AuthService.init)
   .then(createApp)
-  .then((app: Express) => {
-    const server = app.listen(PORT, () =>
-      AppLogger.info(
-        `⚡${ENV_CONFIG.env} server running on port ${ENV_CONFIG.port}`,
-      ),
-    );
-
-    process.on('unhandledRejection', closeServer(server));
-    process.on('uncaughtException', closeServer(server));
-  })
+  .then(createServer)
+  .then(registerServerShutdownListeners)
   .catch((err: unknown) => {
     AppLogger.error(err);
 
